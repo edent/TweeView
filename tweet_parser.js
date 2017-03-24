a39eeb20b84b97d6ea9f747de66b50e92df10172
edent <github.com@shkspr.mobi>
2017-03-24 06:50:06 +0000

    Basic onclick functionality

--- tweet_parser.js
+++ tweet_parser.js
@@ -92,7 +92,17 @@ node.append("image")
 	.attr("xlink:href",function(d) { return d.data.tweet.avatar; })
 	.attr("height", "48px")
 	.attr("width", "48px")
-	.attr("transform","translate(-24 -24)");
+	.attr("transform","translate(-24 -24)")
+	.attr("onclick",function(d) {
+		if (d.parent != null) {
+			return "console.log('" + d.data.tweet.id + " - " +  d.parent.data.tweet.id  +"')";
+		} else {
+			return "console.log('" + d.data.tweet.id + "')";
+		}
+	});
+	// .attr("onclick",function(d) {
+	// 	return "showThread("+d+")";
+	// });
 
 //	Scale and position everything correctly
 var svgWidth = (svg.node().getBBox().width);
